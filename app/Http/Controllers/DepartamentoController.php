aa597da8da45b70dfd22d60c8e5b8b1a8a85e199
Alejandro Parra <jparra102371@gmail.com>
2024-03-29 14:51:44 -0500

    Departamentos add

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -31,7 +31,10 @@ class DepartamentoController extends Controller
      */
     public function create()
     {
-        //
+        $paises = DB::table('tb_pais')
+            ->orderBy('pais_nomb')
+            ->get();
+        return view('departamento.new', ['paises' => $paises]);
     }
 
     /**
@@ -42,7 +45,18 @@ class DepartamentoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $departamento = new Departamento();
+
+        $departamento->muni_nomb = $request->name;
+        $departamento->depa_codi = $request->code;
+        $departamento->save();
+
+        $departamentos = DB::table('tb_departamentos')
+        ->join('tb_pais', 'tb_departamento.pais_codi', '=' , 'tb_pais.pais_codi')
+        ->select('tb_departamento.*' , "tb_pais.pais_nomb")
+        ->get();
+
+        return view('departamento.index', ['departamentos' => $departamentos]);
     }
 
     /**
