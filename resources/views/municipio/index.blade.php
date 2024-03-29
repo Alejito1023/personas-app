809f1f93a70e94f2c6e4e108838f149a506abb89
Alejandro Parra <jparra102371@gmail.com>
2024-03-29 11:51:46 -0500

    Ajuste de la vista del listado de Municipios

--- resources/views/municipio/index.blade.php
+++ resources/views/municipio/index.blade.php
@@ -29,7 +29,7 @@
                 <th scope="row">{{ $municipio->muni_codi}}</th>
                 <td>{{ $municipio->muni_nomb}}</td>
                 <td>{{ $municipio->depa_nomb}}</td>
-                <td><span> Actions </span></td>
+                <td><span> Acciones </span></td>
           </tr>
           @endforeach
         </tbody>
