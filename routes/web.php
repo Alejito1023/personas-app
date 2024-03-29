89bcf12766dd2d5ee780c548cdb21478380eae8a
Alejandro Parra <jparra102371@gmail.com>
2024-03-29 13:01:36 -0500

    Municipio Update

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::get('/municipios' , [MunicipioController::class, 'index'])->name('municip
 Route::post('/municipios' , [MunicipioController::class, 'store'])->name('municipios.store');
 Route::get('/municipios/create' , [MunicipioController::class, 'create'])->name('municipios.create');
 Route::delete('/municipios/{municipio}' , [MunicipioController::class, 'destroy'])->name('municipios.destroy');
+Route::put('/municipios/{municipio}' , [MunicipioController::class, 'update'])->name('municipios.update');
