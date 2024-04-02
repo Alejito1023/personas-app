c2b68161b337eeb35bb37982edc12a8bb4df842a
Alejandro Parra <jparra102371@gmail.com>
2024-04-01 23:44:17 -0500

    Pais update

--- app/Http/Controllers/PaisController.php
+++ app/Http/Controllers/PaisController.php
@@ -91,7 +91,18 @@ class PaisController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $pais = Pais::find($id);
+
+        $pais->pais_nomb = $request->name;
+        $pais->muni_codi = $request->code;
+        $pais->save();
+
+        $paises = DB::table('tb_pais')
+        ->join('tb_municipio', 'tb_pais.pais_codi', '=' , 'tb_municipio.muni_codi')
+        ->select('tb_pais.*', 'tb_municipio.muni_nomb')
+        ->get();
+
+        return view ('pais.index' , ['paises' => $paises]);
     }
 
     /**
