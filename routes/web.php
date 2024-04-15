a4ff230b4e5db782058abcb6953cf8b1f5c821e9
Alejandro Parra <jparra102371@gmail.com>
2024-04-14 20:30:11 -0500

    Proteccion de Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,9 +24,23 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/comunas', [ComunaController::class,
+    'index'])->name('comunas.index');
+
+    Route::post('/comunas', [ComunaController::class,
+    'store'])->name('comunas.store');
+
+    Route::get('/comunas/create', [ComunaController::class,
+    'create'])->name('comunas.create');
+
+    Route::delete('/comunas/{comuna}', [ComunaController::class,
+    'destroy'])->name('comunas.destroy');
+
+    Route::put('/comunas/{comuna}', [ComunaController::class,
+    'update'])->name('comunas.update');
+
+    Route::get('/comunas/{comuna}/edit', [ComunaController::class,
+    'edit'])->name('comunas.edit');
 });
 
 require __DIR__.'/auth.php';
