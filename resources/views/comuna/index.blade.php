02994f4c64551c3c10279fc83a76c9352471cd3d
Alejandro Parra <jparra102371@gmail.com>
2024-04-14 21:07:18 -0500

    Agregar Vista

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -12,9 +12,19 @@
   </head>
 
   <body>
-      <div class="container">
-      <h1> Commune List </h1>
-      <a href=" {{ route("comunas.create") }} " class=" btn btn-success">Add</a>
+      <x-app-layout>
+       <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          {{ ___('Communes') }}
+        </h2> 
+       </x-slot>
+       <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+              <a href="{{ route('comunas.create')}}"
+              class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 
+              rounded ml-2">Add</a>
       <table class="table">
         <thead>
         <tr>
@@ -45,6 +55,11 @@
     @endforeach
   </tbody>
 </table>
+</div>
+</div>
+</div>
+</div>
+</x-app-layout>
 
 
     </div>
